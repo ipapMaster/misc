cfd8a3f7d74a17a94e1b02fd0f9fd35a6ab92ccb
man <a@b.ru>
2023-07-18 15:08:09 +0300

    coroutine

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
-import threading
+def print_name(prefix):
+    print(f'Ищем префикс {prefix}')
+    try:
+        while True:
+            name = (yield)
+            if prefix in name:
+                print(name)
+    except GeneratorExit:
+        print('Корутина (coroutine) завершена')
 
-
-def print_cube(num):
-    """
-    Вычиcляет куб от заданного числа num
-    """
-    print(f'Куб {num} -> {num * num * num}')
-
-
-def print_square(num):
-    """
-    Вычиcляет квадрат от заданного числа num
-    """
-    print(f'Квадрат {num} -> {num ** 2}')
-
-
-if __name__ == '__main__':
-    # создаём два потока
-    thread1 = threading.Thread(target=print_square, args=(10,))
-    thread2 = threading.Thread(target=print_cube, args=(10,))
-
-    thread1.start()  # запуск первого потока
-    thread2.start()  # запуск первого потока
-
-    thread1.join()  # ожидание пока поток 1 завершится
-    thread2.join()  # ожидание пока поток 2 завершится
-
-    print('Процессы завершены')
+corou = print_name('Уважаемый')
+corou.__next__()
+corou.send('товарищ')
+corou.send('Уважаемый товарищ')
+corou.close()
