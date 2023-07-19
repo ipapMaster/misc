7a9a19df2bb7ed6b9fc383c34d3766622b071c49
man <a@b.ru>
2023-07-19 14:38:03 +0300

    Telegram Bot Simple Dialogue

--- main.py
+++ main.py
@@ -4,44 +4,78 @@ from telebot import types  # для указания типов
 
 # /start
 
+name = ''
+surname = ''
+age = 0
+
 # инициировали бота
 bot = telebot.TeleBot(config.bot_token)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id,
-                     text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(
-                         message.from_user), reply_markup=markup)
-
-
 @bot.message_handler(content_types=['text'])
-def func(message):
-    if (message.text == "👋 Поздороваться"):
-        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
-    elif (message.text == "❓ Задать вопрос"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Как меня зовут?")
-        btn2 = types.KeyboardButton("Что я могу?")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
-    elif (message.text == "Как меня зовут?"):
-        bot.send_message(message.chat.id, "У меня нет имени..")
-    elif message.text == "Что я могу?":
-        bot.send_message(message.chat.id, text="Поздороваться с читателями")
-    elif (message.text == "Вернуться в главное меню"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("👋 Поздороваться")
-        button2 = types.KeyboardButton("❓ Задать вопрос")
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+def start(message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id,
+                         "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name)
     else:
-        bot.send_message(message.chat.id, text="На такую команду я не запрограммировал..")
+        bot.send_message(message.from_user.id,
+                         'Я Вас не понял, напишите /start')
+
+
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id,
+                     'Как твоя фамилия имя?')
+    bot.register_next_step_handler(message, get_surname)
+
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id,
+                     'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+
+def get_age(message):
+    global age
+    while age == 0:  # удостоверимся, что поменялся возраст
+        try:
+            age = int(message.text)
+        except ValueError:
+            bot.send_message(message.from_user.id,
+                             'Вводите цифры')
+            age = 1
+            break
+    question = f'Тебе {age} лет, и ты {name} {surname}?'
+    keyboard = types.InlineKeyboardMarkup()  # клавиатура
+    yes = types.InlineKeyboardButton(text='Да',
+                                     callback_data='yes')
+    keyboard.add(yes)
+    no = types.InlineKeyboardButton(text='Нет',
+                                    callback_data='no')
+    keyboard.add(no)
+    bot.send_message(message.from_user.id, text=question,
+                     reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    """
+    call.data - это callback_data, которую
+    мы указали при объявлении кнопки
+    """
+    if call.data == "yes":
+        # тут либо сохраняем, либо обрабатываем данные
+        bot.send_message(call.message.chat.id,
+                         'Приятно познакомиться')
+    elif call.data == "no":
+        # инициируем диалог повторно
+        bot.send_message(call.message.chat.id,
+                         'Тогда начнём сначала... Как тебя зовут?')
+        bot.register_next_step_handler(call.message, get_name)
 
 
 # запустили бота
