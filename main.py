cf1e80b3a72f8302f8440510206300acc484fa77
man <a@b.ru>
2023-07-19 12:15:25 +0300

    Telegram Bot Start

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-import os
-import time
-from datetime import datetime
-import asyncio
+import telebot
+import config
+from telebot import types  # для указания типов
+# /start
 
+# инициировали бота
+bot = telebot.TeleBot(config.bot_token)
+@bot.message_handler(commands=['start']) # реагируем на /start
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    but = types.InlineKeyboardButton('Google',
+                                     url='https://google.com')
+    markup.add(but)
+    bot.send_message(message.chat.id,
+                     'Привет, {0.first_name}! Нажми на кнопку)'.format(message.from_user),
+                     reply_markup=markup)
 
-async def dish(num, prepare, wait):
-    """
-    num: номер блюда по порядку
-    prepare: время на подготовку
-    wait: ожидание готовности
-    """
-    print(f'{datetime.now().strftime("%H:%S")} - подготовка к приготовлению блюда {num} - {prepare} мин.')
-    time.sleep(prepare)
-    print(f'Начало приготовления блюда {num} - {datetime.now().strftime("%H:%S")}. Ожидание блюда {num} {wait} мин.')
-    await asyncio.sleep(wait)
-    print(f'В {datetime.now().strftime("%H:%S")}. блюдо {num} готово.')
-
-
-async def main():
-    tasks = [
-        asyncio.create_task(dish(1, 2, 3)),
-        asyncio.create_task(dish(2, 5, 10)),
-        asyncio.create_task(dish(3, 3, 5))
-    ]
-    await asyncio.gather(*tasks)
-
-
-if __name__ == '__main__':
-    t0 = time.time()  # время начало работы
-    if os.name == 'nt':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())  # запустили асинхронное приготовление
-    delta = int(time.time() - t0)  # затраченное время
-    print(f'В {datetime.now().strftime("%H:%S")} мы закончили')
-    print(f'Затрачено времени - {delta}')
+# запустили бота
+bot.polling(none_stop=True)
