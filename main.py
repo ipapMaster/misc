8aed38644b9ac0128a2662796494309f86f8d657
man <a@b.ru>
2023-07-19 15:26:28 +0300

    Python Telegram Bot Ext (Start and Help)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import logging  # фиксация действий
 import config
 from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import CommandHandler
 
 # Запустим логгирование
 logging.basicConfig(
@@ -22,10 +23,26 @@ async def echo(update, context):
     await update.message.reply_text(update.message.text)
 
 
+async def start(update, context):
+    """
+    Реакция на команду /start
+    """
+    user = update.effective_user
+    await update.message.reply_html(
+        rf'Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-то.'
+    )
+
+
+async def help_command(update, context):
+    await update.message.reply_text('Я пока не сильный помощник')
+
+
 def main():
     application = Application.builder().token(config.bot_token).build()
-    text_handler = MessageHandler(filters.TEXT, echo)
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
     application.add_handler(text_handler)
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help_command))
     application.run_polling()
 
 
