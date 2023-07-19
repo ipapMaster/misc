8a003adf93cf4a4745b50b30f7afc8bc8ae1e3c9
man <a@b.ru>
2023-07-19 12:44:57 +0300

    Telegram Bot New Functions

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 import telebot
 import config
 from telebot import types  # для указания типов
+
 # /start
 
 # инициировали бота
 bot = telebot.TeleBot(config.bot_token)
-@bot.message_handler(commands=['start']) # реагируем на /start
+
+
+@bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.InlineKeyboardMarkup()
-    but = types.InlineKeyboardButton('Google',
-                                     url='https://google.com')
-    markup.add(but)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton("❓ Задать вопрос")
+    markup.add(btn1, btn2)
     bot.send_message(message.chat.id,
-                     'Привет, {0.first_name}! Нажми на кнопку)'.format(message.from_user),
-                     reply_markup=markup)
+                     text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(
+                         message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "👋 Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
+    elif (message.text == "❓ Задать вопрос"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Как меня зовут?")
+        btn2 = types.KeyboardButton("Что я могу?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+    elif (message.text == "Как меня зовут?"):
+        bot.send_message(message.chat.id, "У меня нет имени..")
+    elif message.text == "Что я могу?":
+        bot.send_message(message.chat.id, text="Поздороваться с читателями")
+    elif (message.text == "Вернуться в главное меню"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("👋 Поздороваться")
+        button2 = types.KeyboardButton("❓ Задать вопрос")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text="На такую команду я не запрограммировал..")
+
 
 # запустили бота
 bot.polling(none_stop=True)
