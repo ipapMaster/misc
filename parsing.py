62d4542b68251b93dafca61280dfae3cdaaa1e5a
man <a@b.ru>
2023-07-20 15:42:26 +0300

    Bot Weather

--- parsing.py
+++ parsing.py
@@ -1,21 +1,65 @@
-from bs4 import BeautifulSoup
-import lxml
+import math
+import datetime
 import requests
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-url = 'http://quotes.toscrape.com/'
+bot = Bot(token='5978307749:AAGj48uFMb6NyY2STYJcQxqAcMEs4O6Y_zk')
+dp = Dispatcher(bot)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('span', class_='text')
-authors = soup.find_all('small', class_='author')
-tags = soup.find_all('div', class_='tags')
 
-length = len(quotes)
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
 
-for index in range(length):
-    print(quotes[index].text)
-    print(f'\t\t\t{authors[index].text}')
-    t = tags[index].find_all('a', class_='tag')
-    for item in t:
-        print(f'\t\t\t#{item.text}')
-# print(soup)
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    try:
+        code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
+        response = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&lang=ru&units=metric&appid=your_key")
+        data = response.json()
+        city = data["name"]
+        cur_temp = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+
+        # продолжительность дня
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+        # получаем значение погоды
+        weather_description = data["weather"][0]["main"]
+
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            # если эмодзи для погоды нет, выводим другое сообщение
+            wd = "Посмотри в окно, я не понимаю, что там за погода..."
+        await message.reply(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n"
+                            f"Погода в городе: {city}\nТемпература: {cur_temp}°C {wd}\n"
+                            f"Влажность: {humidity}%\nДавление: {math.ceil(pressure / 1.333)} мм.рт.ст\nВетер: {wind} м/с \n"
+                            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                            f"Хорошего дня!"
+        )
+    except:
+        await message.reply("Проверьте название города!")
+
+
+if __name__ == "__main__":
+    # С помощью метода executor.start_polling опрашиваем
+    # Dispatcher: ожидаем команду /start
+    executor.start_polling(dp)
